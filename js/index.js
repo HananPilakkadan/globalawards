827965195add82b0f5eb325c2f1f18c168e0f16a
Hanan <63251521+HananPilakkadan@users.noreply.github.com>
2021-10-16 23:38:11 +0530

    updated-responsive-screen

--- js/index.js
+++ js/index.js
@@ -12,44 +12,6 @@ $(document).ready(function () {
 
   wow.init();
 
-
-  // image-slider-inited
-  $('.slider').slick({
-    dots: false,
-    infinite: true,
-    speed: 300,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 1000,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 1,
-          infinite: true,
-          dots: false
-        }
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-    ]
-  });
-
-
   // header-sticky
   window.onscroll = function () {
     headerFunction();
